d1ce9a9a6e25090b65654d0b0f6e076456a76bec
nerldy <arnodhis@gmail.com>
2017-09-09 07:00:35 +0300

    random number game init

---
+++ randomNmberGenerator.js
@@ -0,0 +1,13 @@
+// TODO: RANDOM NUMBER GAME
+// to start game, type begin numbers
+// todo: ask for username and password
+// if user name does not exist, ask them to create one
+//todo: check if created user name exists. All user name MUST be unique
+// will need username and password keys
+// todo: generate a random number
+// todo: check it against the user's input
+// if they correct, give them a trophy
+// If they are wrong, tell them they have 2 more attempts and ask for their new input
+//if they run out of attempts, sad face and game over.
+// todo: ask them if they want to play again
+// keep tabs on how many random number games they have played, how many they have won/lost
